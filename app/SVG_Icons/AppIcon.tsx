537646f2b4b3d48cb708ad8cf7fd09ce8bcc0ccc
Edgar Quezada <edgar.quezada@gmail.com>
2025-10-06 16:15:58 +0200

    update appIcon.tsx file

---
+++ app/SVG_Icons/AppIcon.tsx
@@ -0,0 +1,17 @@
+const AppIcon = ({ color, height, width }: { color: "#000000", width: "64px", height: "64px" }) => (
+    <svg
+        viewBox="0 0 64 64"
+        xmlns="http://www.w3.org/2000/svg"
+        fill={color}
+        width={width}
+        height={height}
+    >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+            <path fill={color} 
+            d="M32,28 0C14.355 0 0 14.355 0 32s14.355 32 32 32 32-14.355 32-32S49.645 0 32 0zm0 60C16.561 60 4 47.439 4 32S16.561 4 32 4s28 12.561 28 28-12.561 28-28 28z"></path>

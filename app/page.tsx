a88967cd344922923c5d437540b148a5426ad950
Edgar Quezada <edgar.quezada@gmail.com>
2025-10-22 16:25:27 +0200

    update page and AppIcon tsx files

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./mainPageComponents/Navbar";
 export default function Home() {
   return (
     <div>
-    <Navbar />
+      <Navbar />
       <h1 className="poppins font-bold">Habit Tracker Website</h1>
     </div>
   );
